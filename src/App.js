8c901e9e6d8c3d21014dae74802d670cc5ca4d87
Jessica <github email address>
2020-08-27 06:05:12 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import React from 'react';
 import './App.css';
+import DogContainer from './DogContainer'
+import DogCard from './DogCard'
 
-function App() {
-  return (
-    <div className="App">
-      <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
-      </div>
-      <div id="dog-bar">
+class App extends React.Component {
+  state = {
+    dogList: [],
+    dogToBeRendered: "",
+    filterTerm: ""
+  };
 
-      </div>
-      <div id="dog-summary-container">
-        <h1>DOGGO:</h1>
-        <div id="dog-info">
+  componentDidMount(){
+    fetch("http://localhost:3000/pups")
+    .then(r => r.json())
+    .then((dogs) => {
+      this.setState({
+        dogList: dogs
+      })
+    })
+  };
 
+  dogToRender = (dogNameFromChild) => {
+    this.setState({
+      dogToBeRendered: dogNameFromChild
+    })
+  };
+
+  handleFilter = (event) => {
+    this.setState((prevState) => ({
+      filterTerm: !prevState.filterTerm
+    }))
+  };
+
+  render(){ 
+    let {dogList, dogToBeRendered} = this.state
+    return (
+      <div className="App">
+        <div id="filter-div">
+          <button 
+            id="good-dog-filter" 
+            value={this.state.filterTerm} 
+            onClick={this.handleFilter}
+          >
+            Filter good dogs: { this.state.filterTerm === "OFF" ? "OFF" : "ON"}
+          </button>
+        </div>
+        <div id="dog-bar">
+          <DogContainer
+            dogs={dogList}
+            dogToRender={this.dogToRender}
+          />
+        </div>
+        <div id="dog-summary-container">
+          <h1>DOGGO:</h1>
+          <div id="dog-info">
+            <DogCard
+              dogs={dogList}
+              dog={dogToBeRendered}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+};
 
 export default App;
